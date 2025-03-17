d5fb4c89f0c7d28e7c732dfd466fb16495b69b60
Ilya Koldyrkaev <iliyaanderson1337@gmail.com>
2025-03-17 07:51:41 +0300

    create s3_dag

---
+++ train_model/test/config.py
@@ -0,0 +1,4 @@
+open_weather_token = "750e936ddee5b28d5ef42a1730433a6a"
+tg_bot_token = "7785197659:AAGzGBu_eRMrdn8d5SQTBPWsMgDMvo47s00"
+#docker build --tag tgbot:v1.0 .
+#docker run -e open_weather_token=750e936ddee5b28d5ef42a1730433a6a -e tg_bot_token=7785197659:AAGzGBu_eRMrdn8d5SQTBPWsMgDMvo47s00 --name tgbot tgbot:v1.0
